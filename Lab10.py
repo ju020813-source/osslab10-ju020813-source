18ec7c41d83bb14f4d0582f560f302030b8e76ca
ju020813-source <ju020813@gmail.com>
2025-11-09 19:31:12 +0900

    Update Lab10.py

--- Lab10.py
+++ Lab10.py
@@ -1,1 +1,10 @@
+def divisors(n):
+    result = []
+    for i in range(1, n + 1):
+        if n % i == 0:
+            result.append(i)
+    return result
 
+if __name__ == "__main__":
+    n = int(input())
+    print(*divisors(n))
